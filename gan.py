8c6539e281857e31584ca911da063eded6a0c6e0
Dmitri Soshnikov <dmitri@soshnikov.com>
2019-05-03 13:58:29 +0000

    Remove car-specific data, refactor code, add 512 resolution

--- gan.py
+++ gan.py
@@ -18,33 +18,36 @@ from keras.optimizers import Adam
 from tqdm import tqdm
 
 
+
 class DCGAN():
 
-    def __init__(self):
+    def __init__(self,experiment_name='experiment'):
 
-        self.img_rows = 100
-        self.img_cols = 150
+        self.experiment = experiment_name
+        self.img_rows = 512
+        self.img_cols = 512
         self.channels = 3
         self.img_shape = (self.img_rows, self.img_cols, self.channels)
-        self.latent_dim = 100
+        self.latent_dim = 150
+        self.no_samples = 10
 
+        self.ensure_dirs()
 
         optimizer = Adam(0.0001, 0.5)
 
-        '''
-        # uncomment to build discriminator, generator
-        
-        self.discriminator = self.create_discriminator()
-        self.discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
-        self.generator = self.create_generator()
-        
-        '''
+        if os.path.isfile(self.lpath("models\\dis.h5")) and os.path.isfile(self.lpath("models\\gen.h5")):
+            # uncomment to load discriminator, generator
+            print("Loading existing models...")
+            self.discriminator = load_model(self.lpath('models\\dis.h5'))
+            self.discriminator.compile(loss='binary_crossentropy', optimizer=optimizer, metrics=['accuracy'])
+            self.generator = load_model(self.lpath('models\\gen.h5'))
+        else:
+            # build discriminator, generator
+            self.discriminator = self.create_discriminator()
+            self.discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
+            self.generator = self.create_generator()
 
-        # uncomment to load discriminator, generator
-        self.discriminator = load_model('vroum\\vroumdis.h5')
-        self.discriminator.compile(loss='binary_crossentropy',optimizer=optimizer,metrics=['accuracy'])
-        self.generator = load_model('vroum\\vroumgen.h5')
-        
+
         # the combined model take an image as input and output validity from 0 to 1
         # note that in the combined model, the discriminator is not trainable
         self.discriminator.trainable = False
@@ -56,44 +59,27 @@ class DCGAN():
         self.combined = Model(z, valid) 
         self.combined.compile(loss='binary_crossentropy', optimizer=optimizer)
 
+    def lpath(self,s):
+        return os.path.join(self.experiment,s)
+
+    def ensure_dirs(self):
+        print("Creating experiment %s" % self.experiment)
+        for d in ['models','output','temp']:
+            os.makedirs(os.path.join(self.experiment,d),exist_ok=True)
+
     def create_generator(self):
 
         model = Sequential()
 
-        model.add(Dense(6 * 9 * 256, activation="relu", input_dim=self.latent_dim))
-        model.add(Reshape((6, 9, 256)))
+        model.add(Dense(8 * 8 * 512, activation="relu", input_dim=self.latent_dim))
+        model.add(Reshape((8, 8, 512)))
 
-        model.add(Conv2D(256, kernel_size=(3,3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(UpSampling2D())
-        
-        model.add(Conv2D(128, kernel_size=(3,3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(momentum=0.8))
-        
-        model.add(UpSampling2D())
-
-        model.add(ZeroPadding2D(padding = ((0,1),(0,1))))
-
-        model.add(Conv2D(64, kernel_size=(3,3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(UpSampling2D())
-
-        model.add(ZeroPadding2D(padding = (0,(1,0))))
-
-        model.add(Conv2D(32, kernel_size=(3,3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(UpSampling2D())
-
-        model.add(Conv2D(16, kernel_size=(3,3), padding="same"))
-        model.add(Activation("relu"))
-        model.add(BatchNormalization(momentum=0.8))
+        for x in [512,256,128,64,32,16,8]:
+            model.add(Conv2D(x, kernel_size=(3,3), padding="same"))
+            model.add(Activation("relu"))
+            model.add(BatchNormalization(momentum=0.8))
+            if x!=8:
+                model.add(UpSampling2D())
 
         model.add(Conv2D(self.channels, kernel_size=(1,1), padding="same"))
         model.add(Activation("tanh"))
@@ -106,33 +92,20 @@ class DCGAN():
         return Model(noise, img)
 
 
-
-
     def create_discriminator(self):
         
         model = Sequential()
 
-        model.add(Conv2D(32, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(AveragePooling2D())
-        model.add(Dropout(0.2))
+        for i,x in enumerate([32,64,128,256]):
+            if i==0:
+                model.add(Conv2D(x, kernel_size=3, strides=2, input_shape=self.img_shape, padding="same"))
+            else:
+                model.add(Conv2D(x, kernel_size=3, strides=2, padding="same"))
 
-        model.add(Conv2D(64, kernel_size=3, strides=2, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(AveragePooling2D())
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(128, kernel_size=3, strides=2, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.2))
-
-        model.add(Conv2D(256, kernel_size=3, strides=1, padding="same"))
-        model.add(BatchNormalization(momentum=0.8))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(Dropout(0.2))
+            model.add(BatchNormalization(momentum=0.8))
+            model.add(LeakyReLU(alpha=0.2))
+            model.add(AveragePooling2D())
+            model.add(Dropout(0.2))
 
         model.add(Flatten())
         model.add(Dense(1, activation='sigmoid'))
@@ -144,12 +117,12 @@ class DCGAN():
 
         return Model(img, validity)
 
-    
-
-    def train(self, batch_size=128, save_interval=50, save_img_interval=50):
+    def train(self, batch_size=128, save_interval=50, save_img_interval=50, print_interval = 10, train_path='.\\data'):
         
         #get dataset
-        X_train = self.load_dataset('car_img\\*')
+        X_train = self.load_dataset(train_path+'\\*')
+
+        print("Training on {} images".format(len(X_train)))
 
         # ones = label for real images
         # zeros = label for fake images
@@ -157,7 +130,10 @@ class DCGAN():
         zeros = np.zeros((batch_size, 1))
 
         # create some noise to track AI's progression
-        self.noise_pred = np.random.normal(0, 1, (1, self.latent_dim))
+        noise_pred = []
+        for _ in range(self.no_samples):
+            noise_pred.append(np.random.normal(0, 1, (1, self.latent_dim)))
+        self.noise_pred = noise_pred
 
         epoch = 0
         while(1):
@@ -181,62 +157,71 @@ class DCGAN():
             g_loss = self.combined.train_on_batch(noise, ones)
 
             #print loss and accuracy of both trains
-            print ("%d D loss: %f, acc.: %.2f%% G loss: %f" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
-            
+            if epoch % print_interval == 0:
+                print ("%d D loss: %f, acc.: %.2f%% G loss: %f" % (epoch, d_loss[0], 100*d_loss[1], g_loss))
 
             if epoch % save_img_interval == 0:
+                print("Saving images...", end='')
                 self.save_imgs(epoch)
+                print("done")
             
             if epoch % save_interval == 0:
-
-                #self.discriminator.save('gan\\vroum\\vroumdis_'+str(epoch)+'.h5')
-                #self.generator.save('gan\\vroum\\vroumgen_'+str(epoch)+'.h5')
-
-                self.discriminator.save('vroum\\vroumdis.h5')
-                self.generator.save('vroum\\vroumgen.h5')
+                print("Saving models...",end='')
+                # We save models twice to simplify loading - both using epoch-specific name, and using short name
+                # so that dis/gen.h5 models always contain the latest trained epoch
+                self.discriminator.save(self.lpath('models\\dis_'+str(epoch)+'.h5'))
+                self.generator.save(self.lpath('models\\gen_'+str(epoch)+'.h5'))
+                self.discriminator.save(self.lpath('models\\dis.h5'))
+                self.generator.save(self.lpath('models\\gen.h5'))
+                print("done")
 
     def save_imgs(self, epoch):
-
-        gen_img = self.generator.predict(self.noise_pred)
-        confidence = self.discriminator.predict(gen_img)
-
-        # Rescale image to 0 - 255
-        gen_img = (0.5 * gen_img + 0.5)*255
-
-        cv2.imwrite('car\\%d_%f.png'%(epoch, confidence), gen_img[0])
-
+        for i,s in enumerate(self.noise_pred):
+            gen_img = self.generator.predict(s)
+            confidence = self.discriminator.predict(gen_img)
+            # Rescale image to 0 - 255
+            gen_img = (0.5 * gen_img[0] + 0.5)*255
+            cv2.imwrite(self.lpath('output\\img_%d_%d_%f.png'%(i,epoch, confidence)), gen_img)
 
     def load_dataset(self,path):
 
         try:
             # try to load existing X_train
-            X_train = np.load('X_train.npy')
-            print('loaded dataset')
+            print('Loading pre-processed dataset...',end='')
+            X_train = np.load(self.lpath('temp\\data.npy'))
+            print("done")
 
         except:
             # else, build X_train and save it
+            print("Loading original dataset...",end='')
             X_train = []
             dos = glob(path)
 
             for i in tqdm(dos):
-                img = cv2.imread(i)
-                img = cv2.resize(img,(self.img_cols, self.img_rows))
+                try:
+                    img = cv2.imread(i)
+                    if img.shape[0]<self.img_rows or img.shape[1]<self.img_cols:
+                        print("Image {} too small - skipping")
+                    elif not(0.8<img.shape[0]/img.shape[1]<1.25):
+                        print("Image {} too skew - skipping")
+                    else:
+                        img = cv2.resize(img,(self.img_cols, self.img_rows))
+                        X_train.append(img)
+                except:
+                    print("Error loading {} - skipping".format(i))
 
-                X_train.append(img)
-
-            cv2.destroyAllWindows()
+            #cv2.destroyAllWindows()
             X_train = np.array(X_train)
 
             # Rescale dataset to -1 - 1
             X_train = X_train / 127.5 - 1
-
-            np.save('X_train.npy',X_train)
-            print('created dataset')
+            print('...saving...',end='')
+            np.save(self.lpath('temp\\data.npy'),X_train)
+            print('done')
             
         return X_train
 
 
 if __name__ == '__main__':
-
-    cgan = DCGAN()
-    cgan.train(batch_size=8, save_interval=5000, save_img_interval=25)
+    cgan = DCGAN(experiment_name='e512')
+    cgan.train(batch_size=1, save_interval=5000, save_img_interval=25, train_path='e:\\art\\data\\flower-painting-sel')
